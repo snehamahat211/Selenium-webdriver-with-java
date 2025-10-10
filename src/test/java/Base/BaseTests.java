02d3337ef59acb2e7483a380cab40ce1da2c8b32
snehamahat211 <snehamahat211@gmail.com>
2025-10-10 14:32:02 +0545

    automated signin

--- src/test/java/Base/BaseTests.java
+++ src/test/java/Base/BaseTests.java
@@ -1,15 +1,23 @@
 package Base;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 
 public class BaseTests {
     private WebDriver driver;
     public void setup(){
         driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get("https://www.youtube.com");
+        driver.manage().window().maximize();
+        WebElement inputsLink=driver.findElement(By.linkText("Sign in"));
+        inputsLink.click();
+//        List<WebElement> links=driver.findElements(By.tagName("style"));
+//        System.out.println(links.size());
         System.out.println(driver.getTitle());
-        driver.quit();
     }
     public static void main(String[] args) {
         BaseTests tests = new BaseTests();
