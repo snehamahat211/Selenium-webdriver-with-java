20d23aef5a02ad64a70ccc6e870c51ddb0c5a3a4
snehamahat211 <snehamahat211@gmail.com>
2025-10-14 18:35:04 +0545

    hover link and title added

--- src/main/java/Pages/HoversPage.java
+++ src/main/java/Pages/HoversPage.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class HoversPage {
     private WebDriver driver;
     private By figureBox= By.id("figure");
+    private By boxCaption =By.id("boxCaption");
 
     public HoversPage(WebDriver driver){
         this.driver=driver;
@@ -20,6 +21,25 @@ public class HoversPage {
         WebElement figure=figures.get(index-1);
         Actions actions=new Actions(driver);
         actions.moveToElement(figure).perform();
+        return new FigureCaption(figure.findElement(boxCaption));
+    }
+    public class FigureCaption(WebElement caption){
+        private WebElement caption;
+        private By header=By.tagName("h5");
+        private By link=By.tagName("a");
+        public FigureCaption(WebElement caption){
+            this.caption=caption;
+        }
+        public boolean isCaptionDisplayed(){
+            return caption.isDisplayed();
+        }
+        public String getTitle(){
+            return caption.findElement(header).getText();
+        }
+        public String getLink(){
+            return caption.findElement(link).getAttribute("href");
+        }
+        public class FigureCaption(...)
     }
 
 
