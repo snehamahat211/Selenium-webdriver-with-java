bfe652b650514d85a0a2b1974d35b9a9463a76e5
snehamahat211 <snehamahat211@gmail.com>
2025-10-15 18:44:33 +0545

    test the field for special charecter

--- src/main/java/Pages/KeyPressesPage.java
+++ src/main/java/Pages/KeyPressesPage.java
@@ -1,6 +1,7 @@
 package Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 public class KeyPressesPage {
@@ -13,6 +14,11 @@ public class KeyPressesPage {
     public void enterText(String text){
         driver.findElement(inputField).sendKeys(text);
     }
+    public void enterPi(){
+        enterText(Keys.chord(Keys.ALT,"p")+"=3.14");
+    }
+
+
     public String getResult(){
         return driver.findElement(resultText).getText();
     }
