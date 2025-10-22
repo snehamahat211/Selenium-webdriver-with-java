76fd3c9d59f2481975cd6da6d555e35a38306b4c
snehamahat211 <snehamahat211@gmail.com>
2025-10-22 21:30:01 +0545

    framework for largedom page

---
+++ src/main/java/Pages/LargeAndDeepDom.java
@@ -0,0 +1,20 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LargeAndDeepDom {
+    private WebDriver driver;
+    private By table =By.id("large-table");
+
+    public LargeAndDeepDom(WebDriver driver){
+        this.driver=driver;
+    }
+    public void scrollToTable(){
+        WebElement tableElement =driver.findElement(table);
+        String script="arguments[0].scrollIntoView(true);";
+        ((JavascriptExecutor)driver).executeScript(script,table);
+    }
+}
