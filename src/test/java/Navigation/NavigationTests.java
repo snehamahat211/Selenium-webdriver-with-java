2303d063cb15fda0ab5d9bc7d589065ec6a48b15
snehamahat211 <snehamahat211@gmail.com>
2025-10-29 15:34:56 +0545

    navigatetests created

---
+++ src/test/java/Navigation/NavigationTests.java
@@ -0,0 +1,14 @@
+package Navigation;
+
+import Base.BaseTests;
+import org.testng.annotations.Test;
+
+public class NavigationTests extends BaseTests {
+    @Test
+    public void  testNavigator(){
+        homePage.clickDynamicLoading().clickExample1();
+
+
+    }
+
+}
